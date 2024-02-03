2ad6ecdbf6173566e862556e1aba69fda3f16409
Tedros Alem <tedsaasfaha@gmail.com>
2024-02-03 19:36:17 +0300

    valid anagram strings solution is implemented using hash map dictionary technique

---
+++ String/valid_anagram.py
@@ -0,0 +1,43 @@
+# demonstration of Valid Anagram problem solution
+# An Anagram is a word or phrase formed by rearranging the letters of a
+# different word or phrase, typically using all the original letters exactly once.
+# Problem:
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# Example:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+#  Approach: Hash map technique
+
+
+def isAnagram(s, t):
+    hash_table = {}
+
+    # Iterate through first string 's' and store each character in the hash_table dictionary with its count as value
+    for char in s:
+        if char not in hash_table:
+            hash_table[char] = 1
+        else:
+            hash_table[char] += 1
+
+    # Now we need to check whether second string 't' is found in the hash_table dictionary
+    for char in t:
+        if char not in hash_table:
+            return False
+        else:
+            hash_table[char] -= 1
+
+    # If any value is non-zero then ith character of both strings are not same
+    for key in hash_table.values():
+        if key != 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    s = "anagram"
+    t = "nagaram"
+    print(isAnagram(s, t))
+    s = "rat"
+    t = "car"
+    print(isAnagram(s, t))
